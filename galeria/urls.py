ffb8796cf27ba3ad50a348b885f371c2b3772be8
Adriano carneiro <adriano.santos@sciensa.com>
2022-10-29 13:28:08 -0300

    finalizando treinamento inicial django

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
